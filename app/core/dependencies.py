4e4a6ccbb89de090fb829407ada6e579987eb579
Fran <117905964+franrmueller@users.noreply.github.com>
2025-05-02 10:43:54 +0200

    Dependencies Login

---
+++ app/core/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, HTTPException, status
+from app.core.security import get_current_user
+
+# STUDENT-only Zugriff
+async def get_current_student_user(user: dict = Depends(get_current_user)):
+    if user["role"] != "STUDENT":
+        raise HTTPException(status_code=403, detail="Nur für Studenten erlaubt.")
+    return user
+
+# PROFESSOR-only Zugriff
+async def get_current_professor_user(user: dict = Depends(get_current_user)):
+    if user["role"] != "PROFESSOR":
+        raise HTTPException(status_code=403, detail="Nur für Professoren erlaubt.")
+    return user
+
+
+#TODO: Admin Zugriff
